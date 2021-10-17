d4d356dec424ceee4806e0d10a76d0077a21d454
dominikdziuba <56097555+dominikdziuba@users.noreply.github.com>
2021-10-18 01:08:29 +0200

    Create calc.h

---
+++ simpleCalc/include/calc.h
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int add(int a, int b); //dodawanie
+int substract(int a, int b); //odejmowanie
+double volume(int a, int b, int h, int H); //objętość
+void help(); //dokumentacja
