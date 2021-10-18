f809512e096641e9ab848acea1936c582b75e657
dominikdziuba <56097555+dominikdziuba@users.noreply.github.com>
2021-10-18 17:53:28 +0200

    Update calc.h

--- simpleCalc/include/calc.h
+++ simpleCalc/include/calc.h
@@ -1,8 +1,14 @@
-#include <iostream>
+#pragma once
 
-using namespace std;
+#ifndef MY_HEADER_H
+#define MY_HEADER_H
 
 int add(int a, int b); //dodawanie
 int substract(int a, int b); //odejmowanie
 double volume(int a, int b, int h, int H); //objętość
 void help(); //dokumentacja
+void error_argc();
+void error_negative();
+void error_lack_of_option();
+
+#endif
