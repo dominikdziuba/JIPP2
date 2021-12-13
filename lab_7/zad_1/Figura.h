dd332985b1d1af564dfababf6b3419d2cb18ce77
dominikdziuba <56097555+dominikdziuba@users.noreply.github.com>
2021-12-13 01:37:57 +0100

    lan_7_zad_1

---
+++ lab_7/zad_1/Figura.h
@@ -0,0 +1,13 @@
+#ifndef LAB_7___ZADANIE_1_FIGURA_H
+#define LAB_7___ZADANIE_1_FIGURA_H
+
+#include <string>
+
+class Figura
+{
+protected:
+    std::string nazwa;
+    std::string kolor;
+};
+
+#endif //LAB_7___ZADANIE_1_FIGURA_H
