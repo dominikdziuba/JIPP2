e806762e7b05f20f0ce4b88dd666cb90ea94260b
dominikdziuba <56097555+dominikdziuba@users.noreply.github.com>
2022-01-17 23:52:49 +0100

    Zad1

---
+++ lab_10/zad1/include/Array.h
@@ -0,0 +1,13 @@
+#ifndef ZAD1_LAB10_ZAD1_INCLUDE_ARRAY_H_
+#define ZAD1_LAB10_ZAD1_INCLUDE_ARRAY_H_
+
+template<typename T, int size>
+class Array{
+  T *arr;
+ public:
+  Array();
+  ~Array();
+};
+
+
+#endif
